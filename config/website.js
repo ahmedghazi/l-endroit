9519043062a110508657e796a4c77e7db4d7b220
Ahmed Ghazi <ahmed.ghazi@soixanteseize.com>
2020-06-12 12:28:30 +0200

    sources init

--- config/website.js
+++ config/website.js
@@ -1,10 +1,10 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-  title: 'Site Name', // Navigation and Site Title
+  title: 'L\'ENDROIT FILMS', // Navigation and Site Title
   titleAlt: 'Site Alt', // Title for JSONLD
   description: 'desc here',
   headline: '', // Headline for schema.org JSONLD
-  url: 'https://aeai.eu', // Domain of your site. No trailing slash!
+  url: 'https://lendroit-films.com', // Domain of your site. No trailing slash!
   logo: '/src/images/logo.png', // Used for SEO
   ogLanguage: 'fr_FR', // Facebook Language
 
@@ -12,8 +12,8 @@ module.exports = {
   favicon: 'src/images/icons/favicon.ico', // Used for manifest favicon generation
   shortName: 'aeai', // shortname for manifest. MUST be shorter than 12 characters
   author: 'aeai', // Author for schemaORGJSONLD
-  themeColor: '#0000ff',
-  backgroundColor: '#0000ff',
+  themeColor: '#21211F',
+  backgroundColor: '#21211F',
 
   instagram: '',
   twitter: '', // Twitter Username
--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,8 @@ module.exports = {
         defaultLang: 'fr-fr',
         path: '/preview', // optional, default: /preview
         previews: true, // optional, default: false
-        pages: [{
+        pages: [
+          {
             type: 'Home',
             match: '/',
             path: '/',
@@ -43,17 +44,18 @@ module.exports = {
               'profilepic',
             ],
           },
-          {
-            type: 'Project', // TypeName from prismic
-            match: '/project/:uid', // pages will be generated under this pattern (optional)
-            path: '/project', // placeholder page for unpublished documents
-            component: require.resolve('./src/templates/page-project.jsx'),
-            // sortBy: 'date_ASC', // optional, default: meta_lastPublicationDate_ASC; useful for pagination
-            sharpKeys: [
-              /image|photo|picture/, // (default)
-              'profilepic',
-            ],
-        }],
+          // {
+          //   type: 'Project', // TypeName from prismic
+          //   match: '/project/:uid', // pages will be generated under this pattern (optional)
+          //   path: '/project', // placeholder page for unpublished documents
+          //   component: require.resolve('./src/templates/page-project.jsx'),
+          //   // sortBy: 'date_ASC', // optional, default: meta_lastPublicationDate_ASC; useful for pagination
+          //   sharpKeys: [
+          //     /image|photo|picture/, // (default)
+          //     'profilepic',
+          //   ],
+          // }
+        ],
       },
     },
     {
